a240e7735765fb2fd2d26e531090f79056d2afcd
phonthep aungkanukulwit <phonthep.aungkanukulwit@scbtechx.io>
2022-08-25 22:50:57 +0700

    initial project

---
+++ swiftui-custom-tabbar-animation/swiftui-custom-tabbar-animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-custom-tabbar-animation
+//
+//  Created by MM on 25/8/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-custom-tabbar-animation/swiftui-custom-tabbar-animation/swiftui_custom_tabbar_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_custom_tabbar_animationApp.swift
+//  swiftui-custom-tabbar-animation
+//
+//  Created by MM on 25/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_custom_tabbar_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
